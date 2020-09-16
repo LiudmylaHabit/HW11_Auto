cbf1c3630bc8b38b57cd70032f42f2118625b29d
vohony1 <vohony1@gmail.com>
2020-09-16 23:15:16 +0300

    Implement "Try-catch" to "ConverterForCurrency" class

--- Converter_with_tests/ConverterForCurrency.cs
+++ Converter_with_tests/ConverterForCurrency.cs
@@ -13,7 +13,18 @@ namespace Converter_with_tests
         public decimal DollarConvertion(decimal sumInDollars, decimal rate)
         {
             decimal convertedSum = 0;
-            convertedSum = sumInDollars / rate;
+            try
+            {
+                convertedSum = sumInDollars / rate;
+                if (rate == 0)
+                {
+                    throw new DivideByZeroException();
+                }
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
             return Math.Round(convertedSum, 4);
         }
     }
