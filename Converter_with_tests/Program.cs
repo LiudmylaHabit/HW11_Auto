cc27a81975b4a23d0eb6bfeed6cceacd0fc58e28
vohony1 <vohony1@gmail.com>
2020-09-16 22:52:35 +0300

    Добавьте файлы проекта.

---
+++ Converter_with_tests/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Converter_with_tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
